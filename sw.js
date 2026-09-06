bb23659e0876fd134e66ac319ed37bb07fee8726
Maf0x <ibuguenoviera@gmail.com>
2026-09-06 19:48:29 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ const CACHE_NAME = "nero-audio-v1";
 const assetsToCache = [
   "./index.html",
   "./manifest.json",
-  "./icon.svg",
   "https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js"
 ];
 
